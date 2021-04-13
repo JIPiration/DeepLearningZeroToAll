51fc4979c2707260c1fc782e0bbab4027df75aa9
JIPiration <koreajip@gmail.com>
2021-04-14 03:38:44 +0900

    Add file

---
+++ segmentation/illumination-based-transformations-skin-lesion-segmentation/skimg_local.py
@@ -0,0 +1,16 @@
+import numpy as np
+from warnings import warn
+
+# All code taken from the official scikit-image repository.
+# https://github.com/scikit-image/scikit-image/tree/dfb7a0922a4d4cc4841c74c1657086ca5d8a6b35
+
+__all__ = [
+    "img_as_float32",
+    "img_as_float64",
+    "img_as_float",
+    "img_as_int",
+    "img_as_uint",
+    "img_as_ubyte",
+    "img_as_bool",
+    "dtype_limits",
+]
